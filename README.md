4db6d6f6572a724b2c286007bdd70eecee1f3654
Jesse <jesseponce233@gmail.com>
2023-10-10 00:38:29 -0500

    odin project library demo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # garden
+
+## description
+This is taking a dive into the oden project, uploading challenge code here. This is a simple library book tracker in which the user can add books to a read. 
---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+const myLibrary = [];
+
+
+
+// objhect constructor 
+function Book(title, author, pages, read) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
+}
+// instantiating a new object 
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'no')
+
+console.log(theHobbit);
+
+function addBookToLibrary() {
+    // add a book to display onto the dashboard 
+}
+
+
