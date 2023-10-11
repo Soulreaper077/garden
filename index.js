f15e5c53d8494bb3298339509d4aa7a1b68e9d33
Jesse <jesseponce233@gmail.com>
2023-10-11 00:57:42 -0500

    water

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'no')
 
 console.log(theHobbit);
 
+// variables 
+const addBookButton = document.getElementById("addBookBtn");
+const readButton = document.getElementById("addRead");
+const removeButton = document.getElementById("removeBookbtn");
+
+// Add an event listener to the button
+addBookButton.addEventListener("click", function() {
+    // Your code to check if the button works
+    console.log("Button was clicked!");
+});
+
 function addBookToLibrary() {
     // add a book to display onto the dashboard 
 }
